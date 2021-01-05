e7e1d46ea786abfe62d37d7d31b6b84cea859234
Daniel de Jesús Rosas Pérez <daniel020197s@gmail.com>
2021-01-04 22:08:50 -0600

    Update settings.py

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=i-q(b6$%wyfp8dx^lypudb=000c))(r_*!8ys9ii4irz6&(n#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
